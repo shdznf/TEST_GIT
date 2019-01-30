1121468d40ec7016dae6f9a334a6498e371f3e89
OFFICEKIEV\s.salo <s.salo@temabit.com>
2019-01-30 12:14:20 +0200

    F1 edit file 2

--- BufferDocument.cs
+++ BufferDocument.cs
@@ -4,27 +4,20 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace SA.Classes
 {
-    /// <summary> Типи файлів імпорту </summary>
     public enum ImportFileType
     {
         None = 0,
 
-        /// <summary>NN-MAIN - Налоговая накладная (заголовок) = НН з Сумового обліку</summary>
         InvoiceHeader = 1,
 
-        /// <summary> RK-MAIN  - Корректировка к НН (заголовок) = КН з Сумового обліку</summary>
         CorrectionInvoiceHeader = 2,
 
-        /// <summary> NN-TAB    - Налоговая накладная (табличная часть) </summary>
         InvoiceDetails = 10,
 
-        /// <summary> RK-TAB     - Корректировка к НН  (табличная часть) </summary>
         CorrectionInvoiceDetails = 20,
 
-        /// <summary> KVT-LIST  - Квитанция на документ от ДФС </summary>
         Receipt = 50,
 
-        /// <summary> LIST-DOC – Управляющий файл </summary>
         MainFile = 100,
     }
 
